d0f7b2bab6ddcdc9c9bbc5d60bf6296484c66819
Patrycjusz Mania <worms308@wp.pl>
2019-07-02 12:24:29 +0200

    Core of application [BE]

---
+++ Back end/src/test/java/com/onbank/OnbankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnbankApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
