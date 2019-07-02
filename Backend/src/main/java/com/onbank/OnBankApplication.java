e108b6ce60b316a547dbd58fc2a5c620235c00d4
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2019-07-03 00:14:52 +0200

    OB-2 [BE] Szkielet aplikacji (2D) - code and gitignore corrections and set up H2 Database, mvnw removed

---
+++ Backend/src/main/java/com/onbank/OnBankApplication.java
@@ -0,0 +1,14 @@
+package com.onbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OnBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnBankApplication.class, args);
+    }
+
+}
