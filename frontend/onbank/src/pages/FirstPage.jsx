bc9cfd7e683dc76dce35fb2ff465ccebebf622e7
Mateusz Bonar <mateusz211@vp.pl>
2019-07-02 13:16:49 +0200

    OB-3 [FE] Szkielet aplikacji (2D)- Route

---
+++ frontend/onbank/src/pages/FirstPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FirstPage extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h1>Witaj na stronie głownej</h1>
+         );
+    }
+}
+ 
+export default FirstPage;
