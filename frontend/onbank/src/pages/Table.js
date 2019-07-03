5fb1514652be6ff69b78ca119ac3e8af7bac48b8
Szymon Jablonski <sjablonski.it@gmail.com>
2019-07-03 19:49:34 +0200

    OB-11 [FE] - Added: transaction list

--- frontend/onbank/src/pages/Table.js
+++ frontend/onbank/src/pages/Table.js
@@ -1,64 +1,155 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { makeStyles, createMuiTheme, MuiThemeProvider, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import MUIDataTable from 'mui-datatables';
+
+const AntTabs = withStyles({
+  root: {
+    borderBottom: '1px solid #e8e8e8',
+  },
+  indicator: {
+    backgroundColor: '#1890ff',
+  },
+})(Tabs);
+
+const AntTab = withStyles(theme => ({
+  root: {
+    textTransform: 'none',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(4),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      color: '#40a9ff',
+      opacity: 1,
+    },
+    '&$selected': {
+      color: '#1890ff',
+      fontWeight: theme.typography.fontWeightMedium,
+    },
+    '&:focus': {
+      color: '#40a9ff',
+    },
+  },
+  selected: {},
+}))(props => <Tab disableRipple {...props} />);
 
 const useStyles = makeStyles(theme => ({
   root: {
-    width: '100%',
-    marginTop: theme.spacing(3),
-    overflowX: 'auto',
+    flexGrow: 1,
   },
-  table: {
-    minWidth: 650,
+  typography: {
+    padding: theme.spacing(3),
+  },
+  demo1: {
+    backgroundColor: theme.palette.background.paper,
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
- 
+const datatableData = [
+  [
+    '01.01.2019',
+    'Jan Kowalski 00 0000 0000 0000 0000 0000 0000',
+    'Hajs z YT',
+    'Przelew z rachunku',
+    '50,00 PLN',
+    '999 950,00 PLN',
+  ],
+  [
+    '01.01.2019',
+    'Jan Nowak 00 0000 0000 0000 0000 0000 0000',
+    'Hajs z YT',
+    'Przelew z rachunku',
+    '-50,00 PLN',
+    '999 950,00 PLN',
+  ],
+  [
+    '01.01.2019',
+    'Jan Adamiak 00 0000 0000 0000 0000 0000 0000',
+    'Hajs z YT',
+    'Przelew z rachunku',
+    '50,00 PLN',
+    '999 950,00 PLN',
+  ],
+  [
+    '01.01.2019',
+    'Jan Testowski 00 0000 0000 0000 0000 0000 0000',
+    'Hajs z YT',
+    'Przelew z rachunku',
+    '-50,00 PLN',
+    '999 950,00 PLN',
+  ],
+  [
+    '01.01.2019',
+    'Jan Brostojewski 00 0000 0000 0000 0000 0000 0000',
+    'Hajs z YT',
+    'Przelew z rachunku',
+    '50,00 PLN',
+    '999 950,00 PLN',
+  ],
 ];
 
-export default function SimpleTable() {
+const Table = () => {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  const getMuiTheme = () =>
+    createMuiTheme({
+      overrides: {
+        MUIDataTable: {
+          paper: {
+            boxShadow: 'none',
+          },
+        },
+      },
+    });
+
+  const columns = [
+    { name: 'date', label: 'Data' },
+    { name: 'account', label: 'Odbiorca/Nadawca' },
+    { name: 'description', label: 'Opis operacji' },
+    { name: 'type', label: 'Rodzaj operacji' },
+    { name: 'cost', label: 'Kwota operacji' },
+    { name: 'saldo', label: 'Saldo' },
+  ];
 
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
+    <div className={classes.root}>
+      <div className={classes.demo1}>
+        <AntTabs value={value} onChange={handleChange} centered>
+          <AntTab label="Lista transakcji" />
+          <AntTab label="Oczekujące/Blokady" />
+        </AntTabs>
+        <MuiThemeProvider theme={getMuiTheme()}>
+          <MUIDataTable
+            data={datatableData}
+            columns={columns}
+            options={{
+              selectableRows: false,
+              filterType: 'checkbox',
+              print: false,
+            }}
+          />
+        </MuiThemeProvider>
+      </div>
+    </div>
   );
-}
+};
+
+export default Table;
