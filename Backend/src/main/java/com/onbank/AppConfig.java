985a90afb1ee5fbc4d1498b47ee9aff3300662ce
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2019-07-03 14:21:18 +0200

    OB-6 [BE] REST API - entity and H2 table and connection

--- Backend/src/main/java/com/onbank/AppConfig.java
+++ Backend/src/main/java/com/onbank/AppConfig.java
@@ -2,10 +2,12 @@ package com.onbank;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @PropertySource("file:${app.home}/application.properties")
 @EnableWebMvc
+@EnableJpaRepositories
 @Configuration
 public class AppConfig {
 }
---
+++ Backend/src/main/java/com/onbank/api/Transfer.java
@@ -0,0 +1,35 @@
+package com.onbank.api;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity(name = "transfer")
+public class Transfer {
+    @Id
+    @Column(name="Id", nullable=false, unique = true)
+    private Long id;
+    @Column(name="date", nullable=false)
+    private Date date;
+    @Column(name="name", nullable=false, length=50)
+    private String name;
+    @Column(name="surname", nullable=false, length=100)
+    private String surname;
+    @Column(name="accountnumber", nullable=false, unique = true)
+    @Size(min = 26, max = 28)
+    private String accountNumber;
+    @Column(name="description", nullable=false, length=4000)
+    private String description;
+    @Column(name="typeofoperation", nullable=false, length=200)
+    private String typeOfOperation;
+    @Column(name="ammount", nullable=false)
+    private BigDecimal ammount;
+    @Column(name="accountballance", nullable=false)
+    private BigDecimal accountBallance;
+}
