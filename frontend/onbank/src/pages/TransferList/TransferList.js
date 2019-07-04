31f9c8518322e423871f7b4f26db238e8c9fddbb
Mateusz Bonar <mateusz211@vp.pl>
2019-07-04 14:17:49 +0200

    OB-11 [FE] Conflict

--- frontend/onbank/src/pages/TransferList/TransferList.js
+++ frontend/onbank/src/pages/TransferList/TransferList.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import Table from '../Table'
-//import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import AccountBar from './AccountBar'
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//    marginLeft: "auto",
-//   color:'default'
-//   },
-// }));
+const useStyles = makeStyles(theme => ({
+  root: {
+   marginLeft: "auto",
+ // width:"95%", 
+  color:'default'
+  },
+}));
 
 
 export default function TransferList() {
 
-  //const classes = useStyles();
+  const classes = useStyles();
 
   return(
-   // <Container className={classes.root}>
-   <Container>
+    <Container className={classes.root}>
         <AccountBar/>
         <br/>
           <Table/>
