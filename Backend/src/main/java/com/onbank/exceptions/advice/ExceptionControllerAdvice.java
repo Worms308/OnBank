f203865d7ab21f559dcd4acfa30f3fe0ec38fd2a
Patrycjusz Mania <worms308@wp.pl>
2019-07-04 08:25:58 +0200

    OB-9 [BE] Obsluga bledow (1D) - added date and changed http status

--- Backend/src/main/java/com/onbank/exceptions/advice/ExceptionControllerAdvice.java
+++ Backend/src/main/java/com/onbank/exceptions/advice/ExceptionControllerAdvice.java
@@ -1,23 +1,26 @@
 package com.onbank.exceptions.advice;
 
-import com.onbank.TestEndPoint;
 import com.onbank.exceptions.ExceptionResponse;
+import org.apache.tomcat.jni.Time;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
-    //Nieznany blad
     @ExceptionHandler(Exception.class)
     public ResponseEntity <ExceptionResponse> unknownException(final Exception e) {
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         exceptionResponse.setDescription("Exception msg: " + e.toString());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+        System.err.println(new Timestamp(new Date().getTime()) + "  EXCEPTION: " + e.toString());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
