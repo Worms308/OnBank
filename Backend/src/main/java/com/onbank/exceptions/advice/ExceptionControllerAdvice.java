b385ba60fbcbe29158ac3cd4568aa0410dbd10d0
Patrycjusz Mania <worms308@wp.pl>
2019-07-04 08:33:24 +0200

    OB-9 [BE] Obsluga bledow (1D) - better exceptions messages

--- Backend/src/main/java/com/onbank/exceptions/advice/ExceptionControllerAdvice.java
+++ Backend/src/main/java/com/onbank/exceptions/advice/ExceptionControllerAdvice.java
@@ -1,7 +1,6 @@
 package com.onbank.exceptions.advice;
 
 import com.onbank.exceptions.ExceptionResponse;
-import org.apache.tomcat.jni.Time;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,7 +18,8 @@ public class ExceptionControllerAdvice {
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         exceptionResponse.setDescription("Exception msg: " + e.toString());
-        System.err.println(new Timestamp(new Date().getTime()) + "  EXCEPTION: " + e.toString());
+        System.err.print(new Timestamp(new Date().getTime()) + "  EXCEPTION: ");
+        e.printStackTrace();
         return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
