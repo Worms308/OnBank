d5141cf804357ee49fa747b42e0934877e121535
Szymon Jabłoński <sjablonski.it@gmail.com>
2019-07-04 19:45:38 +0200

    OB-11 [FE] - Modified: Account column in Table C.T.

--- frontend/onbank/src/pages/Transactions/TransactionsTable/ComplitedTransactions/ComplitedTransactions.js
+++ frontend/onbank/src/pages/Transactions/TransactionsTable/ComplitedTransactions/ComplitedTransactions.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Table from 'shared/Table';
+import { Grid } from '@material-ui/core';
 
 const datatableData = [
   [
     '01.01.2019',
-    'Jan Kowalski 00 0000 0000 0000 0000 0000 0000',
+    'Jan Kowalski 00000000000000000000000000',
     'Hajs z YT',
     'Przelew z rachunku',
     '50,00 PLN',
@@ -12,7 +13,7 @@ const datatableData = [
   ],
   [
     '01.01.2019',
-    'Jan Nowak 00 0000 0000 0000 0000 0000 0000',
+    'Jan Nowak 00000000000000000000000000',
     'Hajs z YT',
     'Przelew z rachunku',
     '-50,00 PLN',
@@ -20,7 +21,7 @@ const datatableData = [
   ],
   [
     '01.01.2019',
-    'Jan Adamiak 00 0000 0000 0000 0000 0000 0000',
+    'Jan Adamiak 00000000000000000000000000',
     'Hajs z YT',
     'Przelew z rachunku',
     '50,00 PLN',
@@ -28,7 +29,7 @@ const datatableData = [
   ],
   [
     '01.01.2019',
-    'Jan Testowski 00 0000 0000 0000 0000 0000 0000',
+    'Jan Testowski 00000000000000000000000000',
     'Hajs z YT',
     'Przelew z rachunku',
     '-50,00 PLN',
@@ -36,7 +37,7 @@ const datatableData = [
   ],
   [
     '01.01.2019',
-    `Jan Brostojewski 00 0000 0000 0000 0000 0000 0000`,
+    `Jan Brostojewski 00000000000000000000000000`,
     'Hajs z YT',
     'Przelew z rachunku',
     '50,00 PLN',
@@ -46,26 +47,41 @@ const datatableData = [
 
 const columns = [
   { name: 'date', label: 'Data' },
-  { name: 'account', label: 'Odbiorca/Nadawca' },
+  {
+    name: 'account',
+    label: 'Odbiorca/Nadawca',
+    options: {
+      customBodyRender: value => {
+        const array = value.split(/([0-9]+)/);
+        return (
+          <Grid container>
+            <Grid item xs={12}>{array[0]}</Grid>
+            <Grid item xs={12}>{array[1].match(/[A-Z]{2}|(?:(?:\d{2}|\d{4})(?=(\d{4})*$))/g).join(" ")}</Grid>
+          </Grid>
+        );
+      },
+    },
+  },
   { name: 'description', label: 'Opis operacji' },
   { name: 'type', label: 'Rodzaj operacji' },
   {
     name: 'cost',
     label: 'Kwota operacji',
     options: {
-      customBodyRender: (value, tableMeta, updateValue) => {
+      customBodyRender: value => {
         if (value < '0') {
           return <span style={{ color: '#C0392B' }}>{value}</span>;
         }
         if (value > '0') {
           return <span style={{ color: '#3FD07C' }}>{value}</span>;
         }
+        return value;
       },
     },
   },
   { name: 'saldo', label: 'Saldo' },
 ];
 
-const ComplitedTransactions = () => <Table data={datatableData} columns={columns} />;
+const ComplitedTransactions = () => <Table data={datatableData} columns={columns}/>;
 
 export default ComplitedTransactions;
