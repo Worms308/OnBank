489ec89df39f7a5cfe64baa7ae0b5ea2aab6d30c
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2019-07-08 12:51:42 +0200

    OB-6 [BE] REST API - first tests

---
+++ Backend/src/test/java/com/onbank/LoadProperties.java
@@ -0,0 +1,15 @@
+package com.onbank;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(TestPropertiesExtensions.class)
+public @interface LoadProperties {
+
+}
