f5edc929ddea7039c484d8548c044774b7afafa9
Patrycjusz Mania <worms308@wp.pl>
2019-07-09 10:18:58 +0200

    OB-18 [BE] Dodawanie przelewu REST - added post method

---
+++ Backend/src/main/java/com/onbank/api/dto/CreateTransferDto.java
@@ -0,0 +1,20 @@
+package com.onbank.api.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+public class CreateTransferDto {
+    private LocalDateTime date;
+    private String name;
+    private String surname;
+    private String accountNumber;
+    private String description;
+    private String typeOfOperation;
+    private BigDecimal ammount;
+
+}
--- Backend/src/main/java/com/onbank/api/service/implementation/TransferServiceImpl.java
+++ Backend/src/main/java/com/onbank/api/service/implementation/TransferServiceImpl.java
@@ -3,21 +3,26 @@ package com.onbank.api.service.implementation;
 import com.onbank.api.model.Transfer;
 import com.onbank.api.repository.TransferRepository;
 import com.onbank.api.service.TransferService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TransferServiceImpl implements TransferService {
 
-    private TransferRepository transferRepository;
+    private final TransferRepository transferRepository;
 
-    public TransferServiceImpl(TransferRepository transferRepository){
-        this.transferRepository = transferRepository;
-    }
-
+    @Override
     public List<Transfer> getTransfers(){
         return transferRepository.findAll();
+    }
+
+    @Override
+    public Transfer setTransfer(Transfer transfer){
+        return transferRepository.save(transfer);
     }
 
 }
