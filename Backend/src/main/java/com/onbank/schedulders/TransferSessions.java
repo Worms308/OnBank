4aef6c16a5b3aa5b42a5c0cc910ff12da8f8c37e
Przemysław Pyk <pyk.przemyslaw@gmail.com>
2019-07-10 10:46:06 +0200

    OB-21 [BE] Created 4 schedulders

---
+++ Backend/src/main/java/com/onbank/schedulders/TransferSessions.java
@@ -0,0 +1,29 @@
+package com.onbank.schedulders;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransferSessions {
+
+    @Scheduled(cron = "0 30 8 * * ?")
+    public void outgoingTransferMorningSession() {
+        System.out.println("Hello darkness my old friend- outgoing morning");
+    }
+
+    @Scheduled(cron = "0 0 10 * * ?")
+    public void incomingTransferMorningSession() {
+        System.out.println("Hello darkness my old friend - incoming morning");
+    }
+
+    @Scheduled(cron = "0 30 14 * * ?")
+    public void outgoingTransferDailySession() {
+        System.out.println("Hello darkness my old friend - outgoing daily");
+    }
+
+    @Scheduled(cron = "0 0 16 * * ?")
+    public void incomingTransferDailySession() {
+        System.out.println("Hello darkness my old friend - incoming daily");
+    }
+
+}
