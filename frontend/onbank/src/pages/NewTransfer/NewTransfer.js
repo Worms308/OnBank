125147819b037387030512c8f9425b295fd394e9
Mateusz Bonar <mateusz211@vp.pl>
2019-07-10 13:03:15 +0200

    OB-13 Fix

--- frontend/onbank/src/pages/NewTransfer/NewTransfer.js
+++ frontend/onbank/src/pages/NewTransfer/NewTransfer.js
@@ -3,94 +3,129 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import Radio from '@material-ui/core/Radio';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import Checkbox from '@material-ui/core/Checkbox';
+import NumberFormat from 'react-number-format';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import PermContactCalendar from '@material-ui/icons/PermContactCalendar';
+//import AccountNumberFormat from 'core/AccountNumberFormat';
 
 const useStyles = makeStyles(theme => ({
     root: {
-      height:"90%",
       paddingTop:20,
     },
-
+    recieverField:{
+      marginLeft:'33%',
+      width:'26%',
+      [theme.breakpoints.down('sm')]: {
+        marginLeft:'10%',
+        width:'50%',
+      },
+    },
     typographyText:{
-        marginLeft:'30%',
-        whiteSpace: 'nowrap'
+        marginLeft:'33%',
+        whiteSpace: 'nowrap',
+        [theme.breakpoints.down('sm')]: {
+            marginLeft:'10%',
+        },
     },
     textField:{
-        marginLeft:'30%',
+        marginLeft:'33%',
         marginTop:0,
         width:'30%',
         marginBottom:'2%',
-        whiteSpace: 'nowrap'
+        whiteSpace: 'nowrap',
+        [theme.breakpoints.down('sm')]: {
+            marginLeft:'10%',
+            width:'70%',
+          },
     },
     textFieldAmount:{
-        marginLeft:'30%',
+        marginLeft:'33%',
         marginTop:0,
         width:'10%',
         marginBottom:'2%',
         whiteSpace: 'nowrap',
         display:'inline-block',
+        [theme.breakpoints.down('sm')]: {
+            marginLeft:'10%',
+            width:'70%',
+        },
     },
-    textPLN:{
-        marginLeft:'1%',
-        display:'inline-block',
-        fontSize:15,
-    },
-    radioButtonTextElixir:{
-         marginLeft:'31%',
-         display:'inline-block'
-    },
-    radioButtonText:{
-        marginLeft:'3%',
-        display:'inline-block'
-   },
-    radioButton1:{
-        marginLeft:'30%',
+    divButton:{
+        marginLeft:'33%',
+        paddingBottom:20,
         display:'inline-block'
     },
-    radioButton2:{
-        marginLeft:'6%',
+    icon:{
+      fontSize:50
+    },
+    checkbox:{
+      marginLeft:'32%',
     },
     button:{
-       // float:"right",
-        marginBottom:'5%',
-        marginLeft:'70%'
-
+      marginLeft:'55%',
+      marginBottom:'2%'
     }
+   
   }));
+
+  function NumberFormatCustom(props) {
+    const { inputRef, onChange, ...other } = props;
+  
+    return (
+      <NumberFormat
+        {...other}
+        getInputRef={inputRef}
+        onValueChange={values => {
+          onChange({
+            target: {
+              value: values.value,
+            },
+          });
+        }}
+        thousandSeparator
+        suffix=" PLN"
+      />
+    );
+  }
+
+  NumberFormatCustom.propTypes = {
+    inputRef: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+  };
 
 export default function NewTransfer()
 {
+
     const classes = useStyles();
     const [values, setValues] = React.useState({
-        name: 'Cat in the Hat',
+        name: '',
+        
       });
-    const [selectedValue, setSelectedValue] = React.useState('a');
+
     const handleChange = name => event => {
         setValues({ ...values, [name]: event.target.value });
       };
-      function handleChangeRadio(event) {
-        setSelectedValue(event.target.value);
-      }
-
-   
+      
     return(
        <Paper className={classes.root}>
-        
+         
+         <div className={classes.divInputs}>
             <TextField
             id="standard-name"
             label="Odbiorca"
-            className={classes.textField}
+            className={classes.recieverField}
             onChange={handleChange('name')}
-           
         />
+        <PermContactCalendar className={classes.icon}></PermContactCalendar>
         
             <TextField
             id="standard-name"
             label="Nr konta"
             className={classes.textField}
             onChange={handleChange('name')}
-           
         />
          
             <TextField
@@ -101,47 +136,42 @@ export default function NewTransfer()
            
         />
         <Typography variant="subtitle1" className={classes.typographyText}>Kwota:</Typography>
-            <TextField
-            id="standard-name"
-            label="Podaj kwotę"
+        <TextField
+            className={classes.formControl}
+            label="0,00 PLN"
+            min="0"
+            value={values.numberformat}
+            onChange={handleChange('numberformat')}
             className={classes.textFieldAmount}
-            onChange={handleChange('name')}
-           
-        />
-        <h4 className={classes.textPLN}>PLN</h4>
-            <TextField
-                id="date"
-                label="Data przelewu"
-                type="date"
-                className={classes.textField}
-                InputLabelProps={{
-                shrink: true,
+            InputProps={{
+            inputComponent: NumberFormatCustom,
             }}
         />
-        <h4 className={classes.typographyText}>Typ przelewu</h4>
-        <h5 className={classes.radioButtonTextElixir}>Elixir</h5>
-        <h5 className={classes.radioButtonText}>Przelew natychmiastowy</h5>
         <br/>
-            <Radio
-            checked={selectedValue === 'a'}
-            onChange={handleChangeRadio}
-            value="a"
-            className={classes.radioButton1}
-            color="default"
-            />
-           
-        <Radio
-            checked={selectedValue === 'b'}
-            onChange={handleChangeRadio}
-            value="b"
-            name="radio-button-demo"
-            color="default"
-            className={classes.radioButton2}
-         />
+        <TextField
+        id="date"
+        label="Data przelewu"
+        type="date"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <br/>
+        <FormControlLabel
+          value="start"
+          control={<Checkbox color="default" />}
+          label="Zapamiętaj odbiorcę"
+          labelPlacement="end"
+          className={classes.checkbox}
+        />
+      </div> 
          <br/>
          <Button variant="contained" color="primary" className={classes.button}>
             Wyślij
          </Button>
+    
+
        </Paper>
     )
 }
