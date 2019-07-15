03e1f9e1521570dab376fe4f6bf8fc8eed405abc
Patrycjusz Mania <worms308@wp.pl>
2019-07-15 13:53:41 +0200

    OB-28 [BE] Encje uzytkownika - added foreign key and validation

--- Backend/src/main/java/com/onbank/api/model/User.java
+++ Backend/src/main/java/com/onbank/api/model/User.java
@@ -6,8 +6,13 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Entity
@@ -22,12 +27,15 @@ public class User extends EntityCore {
     private String surname;
 
     @Column(name = "phone")
+    @Size(min = 9, max = 11, message = "Phone number must be between 9-11 numbers.")
     private String phone;
 
     @Column(name = "email", nullable = false)
+    @Email
     private String email;
 
     @Column(name = "pesel", nullable = false, unique = true)
+    @Size(min = 11, max = 11)
     private String pesel;
 
     @Column(name = "nationality", nullable = false)
@@ -37,6 +45,7 @@ public class User extends EntityCore {
     private String birthPlace;
 
     @Column(name = "birthdate", nullable = false)
+    @Past
     private LocalDate birthDate;
 
     @Column(name = "mothermaidenname", nullable = false)
@@ -45,5 +54,6 @@ public class User extends EntityCore {
     @Column(name = "idNumber", nullable = false)
     private String idNumber; // NUMER DOWODU OSOBISTEGO
 
-//    private List<Transfer> transfers;
+    @OneToMany
+    private List<Transfer> transfers;
 }
