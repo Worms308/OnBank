cad104569bff11e8ff4e23c8c5ca9bf0ab03b3f2
Patrycjusz Mania <worms308@wp.pl>
2019-07-16 14:57:18 +0200

    OB-36 - without classpath again

--- Backend/src/main/java/com/onbank/AppConfig.java
+++ Backend/src/main/java/com/onbank/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@PropertySource("file:${app.home}/application.properties")
+//@PropertySource("file:${app.home}/application.properties")
 @EnableWebMvc
 @Configuration
 public class AppConfig {
