186c05af4bd6ed740d856ca55714559b7f3e18fd
Patrycjusz Mania <worms308@wp.pl>
2019-07-16 08:55:09 +0200

    OB-29 [BE] Starter - last refactor

--- Backend/src/main/java/com/onbank/starter/InitMockDB.java
+++ Backend/src/main/java/com/onbank/starter/InitMockDB.java
@@ -14,11 +14,11 @@ import java.time.LocalDate;
 @Component
 public class InitMockDB {
 
-    private static final String MARIUSZ_KOWALSKI = "Mariusz Kowalski";
-    private static final String PL_61306662783096158101751159 = "PL61306662783096158101751159";
+    private static final String ACCOUNT_NAME_1 = "Mariusz Kowalski";
+    private static final String ACCOUNT_NUMBER_1 = "PL61306662783096158101751159";
 
-    private static final String ŁUKASZ_NOWAK = "Łukasz Nowak";
-    private static final String PL_57665182618431955785589025 = "PL57665182618431955785589025";
+    private static final String ACCOUNT_NAME_2 = "Łukasz Nowak";
+    private static final String ACCOUNT_NUMBER_2 = "PL57665182618431955785589025";
 
     @Autowired
     private TransferRepository transferRepository;
@@ -36,119 +36,119 @@ public class InitMockDB {
     private void initTransfers(){
         transferRepository.save(new Transfer(
                 LocalDate.of(2019, 03, 23),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Przykładowy przelew z utf-8.",
                 OperationType.NORMAL,
                 new BigDecimal("150.0"),
                 new BigDecimal("2400.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2019, 05, 11),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Przelew za zakupy.",
                 OperationType.NORMAL,
                 new BigDecimal("150.0"),
                 new BigDecimal("2250.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2019, 07, 29),
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025,
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2,
                 "Sprzedaż oprogramowania.",
                 OperationType.NORMAL,
                 new BigDecimal("1150.0"),
                 new BigDecimal("3000.0"),
                 TransferState.REALIZED,
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2018, 12, 24),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Prezent gwiazdkowy.",
                 OperationType.NORMAL,
                 new BigDecimal("150.0"),
                 new BigDecimal("2000.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2018, 2, 1),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Przelew na piwo.",
                 OperationType.NORMAL,
                 new BigDecimal("15.0"),
                 new BigDecimal("2000.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2018, 6, 25),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Prezent urodzinowy.",
                 OperationType.NORMAL,
                 new BigDecimal("15.0"),
                 new BigDecimal("2000.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2018, 11, 10),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Kredyt na ogrzewanie",
                 OperationType.NORMAL,
                 new BigDecimal("1500.0"),
                 new BigDecimal("3000.0"),
                 TransferState.REALIZED,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2019, 2, 28),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Przelew bez okazji.",
                 OperationType.NORMAL,
                 new BigDecimal("150.0"),
                 new BigDecimal("2000.0"),
                 TransferState.WAITING,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
 
         transferRepository.save(new Transfer(
                 LocalDate.of(2019, 7, 8),
-                MARIUSZ_KOWALSKI,
-                PL_61306662783096158101751159,
+                ACCOUNT_NAME_1,
+                ACCOUNT_NUMBER_1,
                 "Pieniądze dla studentów",
                 OperationType.NORMAL,
                 new BigDecimal("1.0"),
                 new BigDecimal("20.0"),
                 TransferState.WAITING,
-                ŁUKASZ_NOWAK,
-                PL_57665182618431955785589025
+                ACCOUNT_NAME_2,
+                ACCOUNT_NUMBER_2
         ));
     }
 }
