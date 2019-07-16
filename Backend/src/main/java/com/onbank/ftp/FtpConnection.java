4cb8a04b3cb005d6428eba7ba9c860fa0b904498
Przemysław Pyk <przempy@gmail.com>
2019-07-16 10:40:12 +0200

    Feature/ob 23 - ftp connection (#18)
    * OB-21 [BE] Created 4 schedulders
    
    * OB-23 [BE] First commit
    
    * OB-23 [BE] - ftp connection
    
    * Merge branch 'origin' into feature/OB-23
    
    # Conflicts:
    #	Backend/src/main/java/com/onbank/OnBankApplication.java
    
    * Feature/ob 23 - ftp connection correction
    
    * Feature/ob 23 - ftp connection correction
    
    * Feature/ob 23 - ftp connection correction
    
    * Feature/ob 23 - ftp connection correction
    
    * Feature/ob 23 - ftp connection correction

---
+++ Backend/src/main/java/com/onbank/ftp/FtpConnection.java
@@ -0,0 +1,52 @@
+package com.onbank.ftp;
+
+import lombok.Setter;
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+
+@Component
+@Setter
+public class FtpConnection {
+
+    @Value("${onbank.server}")
+    private String server;
+    @Value("${onbank.port}")
+    private int port;
+    @Value("${onbank.user}")
+    private String user;
+    @Value("${onbank.password}")
+    private String password;
+    private FTPClient ftp;
+
+    public void open() throws IOException {
+        ftp = new FTPClient();
+
+        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
+
+        ftp.connect(server, port);
+        int reply = ftp.getReplyCode();
+        if (!FTPReply.isPositiveCompletion(reply)) {
+            ftp.disconnect();
+            throw new IOException("Exception in connecting to FTP Server");
+        }
+        ftp.login(user, password);
+    }
+
+    public void close() throws IOException {
+        ftp.disconnect();
+    }
+
+    public void downloadFile(String source, String destination) throws IOException {
+        FileOutputStream out = new FileOutputStream(destination);
+        ftp.retrieveFile(source, out);
+    }
+
+    public void uploadFile(File file, String path) throws IOException {
+        ftp.storeFile(path, new FileInputStream(file));
+    }
+}
--- Backend/src/main/java/com/onbank/schedulers/TransferSessions.java
+++ Backend/src/main/java/com/onbank/schedulers/TransferSessions.java
@@ -1,19 +1,38 @@
 package com.onbank.schedulers;
 
+import com.onbank.ftp.FtpConnection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class TransferSessions {
 
+    @Autowired
+    FtpConnection ftpConnection;
+
 
     @Scheduled(cron = "${onbank.outgoing.transfer.date}")
     public void outgoingTransferMorningSession() {
-        System.out.println("Hello darkness my old friend- outgoing");
+        System.out.println("Session outgoing");
+        try {
+            ftpConnection.open();
+            ftpConnection.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 
     @Scheduled(cron = "${onbank.incoming.transfer.date}")
     public void incomingTransferMorningSession() {
-        System.out.println("Hello darkness my old friend - incoming");
+        System.out.println("Session incoming");
+        try {
+            ftpConnection.open();
+            ftpConnection.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
