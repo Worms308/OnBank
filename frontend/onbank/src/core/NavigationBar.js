84327e2304ab824cde06725285f9cb70a648b906
Szymon Jablonski <sjablonski.it@gmail.com>
2019-07-16 11:14:46 +0200

    OB-31 [FE] - Fixed: useEffect warning;

--- frontend/onbank/src/core/NavigationBar.js
+++ frontend/onbank/src/core/NavigationBar.js
@@ -40,7 +40,7 @@ const NavigationBar = ({ location, userProfile, getUserProfile }) => {
   const [disabledValue, setDisabledValue] = React.useState(true);
   const [disabledValue2, setDisabledValue2] = React.useState(false);
 
-  useEffect(() => getUserProfile(userProfile.id), []);
+  useEffect(() => getUserProfile(userProfile.id), [getUserProfile, userProfile.id]);
 
   const toggleDrawer = (side, open) => event => {
     if (event.type === 'keydown') {
