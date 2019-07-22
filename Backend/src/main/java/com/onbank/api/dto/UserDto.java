a76fc80c61e7e88a3cf0c3fb42765abffa4c994a
Patrycjusz Mania <worms308@wp.pl>
2019-07-22 10:48:22 +0200

    OB - 34 - added mocks to user logic

--- Backend/src/main/java/com/onbank/api/dto/UserDto.java
+++ Backend/src/main/java/com/onbank/api/dto/UserDto.java
@@ -7,4 +7,5 @@ public class UserDto {
     private Long id;
     private String name;
     private String surname;
+    private String accountNumber;
 }
--- Backend/src/main/java/com/onbank/api/repository/TransferRepository.java
+++ Backend/src/main/java/com/onbank/api/repository/TransferRepository.java
@@ -4,5 +4,9 @@ import com.onbank.api.model.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface TransferRepository extends JpaRepository<Transfer, Long> {}
+public interface TransferRepository extends JpaRepository<Transfer, Long> {
+    List<Transfer> getTransfersBySenderAccountNumberOrRecipientAccountNumber(String senderAccountNumber, String recipientAccountNumber);
+}
