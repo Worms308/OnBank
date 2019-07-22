13f07719a7afdbac66506f841a0af93f9f7f2dc3
Patrycjusz Mania <worms308@wp.pl>
2019-07-22 12:28:31 +0200

    OB-27 - hotfix repository

--- Backend/src/main/java/com/onbank/api/repository/TransferRepository.java
+++ Backend/src/main/java/com/onbank/api/repository/TransferRepository.java
@@ -12,5 +12,6 @@ import java.util.List;
 public interface TransferRepository extends JpaRepository<Transfer, Long> {
 
     List<Transfer> findByRealizationStateAndDateAfter(TransferState realizationState, LocalDate firstDate);
+    List<Transfer> getTransfersBySenderAccountNumberOrRecipientAccountNumber(String senderAccountNumber, String recipientAccountNumber);
 
 }
