4ff62fabd0a07c92306fd37084ef826d0a019ea0
Worms308 <worms308@wp.pl>
2019-07-22 16:07:45 +0200

    OB-43 remove mock comment

--- Backend/src/main/java/com/onbank/Mocks.java
+++ Backend/src/main/java/com/onbank/Mocks.java
@@ -3,7 +3,7 @@ package com.onbank;
 import com.onbank.api.model.UserMock;
 
 public class Mocks {
-
+    //TODO remove mock
     public static UserMock getMockUser() {
         return new UserMock();
     }
